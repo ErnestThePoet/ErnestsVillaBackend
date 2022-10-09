551ea1dfc367c018bd022e9d3358f3f835817ab9
Ernest Cui <ecuiships@126.com>
2022-10-09 09:43:39 +0800

    init

---
+++ src/main/java/com/ecui/ErnestsVilla/ErnestsVillaApplication.java
@@ -0,0 +1,13 @@
+package com.ecui.ErnestsVilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErnestsVillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErnestsVillaApplication.class, args);
+	}
+
+}
